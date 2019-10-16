a8d76637a414256a1c05e4eaf279cfe7b3ef142f
Dapeng Liu <qq___2000@126.com>
2019-10-16 22:59:51 +0200

    async supported

--- client/agent-lib/src/JobWorker.ts
+++ client/agent-lib/src/JobWorker.ts
@@ -4,12 +4,27 @@ const ctx: Worker = self as any;
 
 ctx.addEventListener("message", (event) => {
     console.log('job worker: job received');
-    const res = doCalculation(event.data);
-    console.log('job worker: job finish');
-    ctx.postMessage(res);
+    doCalculation(event.data).then(res => {
+        console.log('job worker: job finish');
+        ctx.postMessage(res);
+    });
 });
 
-function doCalculation(req: CalculationRequest): CalculationResponse {
+function evalScript(script: string): Promise<any> {
+    function evalFull() {
+        try {
+            const begin = "(function fun(){";
+            const end = "})()";
+            return eval(begin + script + end)
+        } catch (e) {
+            return e.message;
+        }
+    }
+
+    return Promise.resolve(evalFull());
+}
+
+function doCalculation(req: CalculationRequest): Promise<CalculationResponse> {
     if (typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScope) {
         console.log('running in web worker')
     }
@@ -18,15 +33,19 @@ function doCalculation(req: CalculationRequest): CalculationResponse {
         result: "",
         state: CalculationState.SUCCESS
     };
-    try {
-        console.log('calculation job received, id = ', req.id);
-        res.result = JSON.stringify(eval(req.script));
-        console.log('calculation job finished, id = ', req.id);
-    } catch (e) {
-        res.result = e.message;
-        res.state = CalculationState.ERROR;
-    }
-    return res;
+
+    console.log('calculation job received, id = ', req.id);
+    return evalScript(req.script)
+        .then(data => {
+            res.result = JSON.stringify(data);
+            console.log('calculation job finished, id = ', req.id);
+            return res;
+        })
+        .catch(e => {
+            res.result = JSON.stringify(e);
+            console.log('calculation job finished with error, id = ', req.id);
+            return res;
+        });
 }
 
 
